4a1340be5d2b3375a0ba5bb065dfeb0c40657611
Mingzhi Xu <mingzhi22@yahoo.com>
2019-04-27 22:46:37 -0400

    Add files via upload

--- Test1/site1/home/views.py
+++ Test1/site1/home/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 from home.forms import HomeForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 class HomeView(TemplateView):
 	template_name = 'home/home.html'
@@ -14,6 +14,7 @@ class HomeView(TemplateView):
 		if form.is_valid():
 			text = form.cleaned_data['post']
 			form = HomeForm()
+			
 			
 		args = {'form': form, 'text': text}
 		return render(request, self.template_name, args)
