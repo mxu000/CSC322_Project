d3e55503a5f2bf2b3ef96a69694678fdecc5a987
Mingzhi Xu <mingzhi22@yahoo.com>
2019-04-29 07:55:31 -0400

    Add files via upload

--- Test1/site1/home/models.py
+++ Test1/site1/home/models.py
@@ -7,3 +7,20 @@ class Post(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	created_date = models.DateTimeField(auto_now_add=True)
 	updated_date = models.DateTimeField(auto_now=True)
+	
+class Friend(models.Model):
+	users = models.ManyToManyField(User, blank=True)
+	current_user = models.ForeignKey(User, related_name='owner', null=True, on_delete=models.CASCADE)
+	@classmethod
+	def add_friend(cls, current_user, new_friend):
+		friend, created = cls.objects.get_or_create(
+			current_user = current_user
+		)
+		friend.users.add(new_friend)
+		
+	@classmethod
+	def remove_friend(cls, current_user, new_friend):
+		friend, created = cls.objects.get_or_create(
+			current_user = current_user
+		)
+		friend.users.remove(new_friend)
--- Test1/site1/home/views.py
+++ Test1/site1/home/views.py
@@ -1,8 +1,9 @@
 from django.views.generic import TemplateView
-from home.forms import HomeForm
-from home.models import Post
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+
+from home.forms import HomeForm
+from home.models import Post, Friend
 
 class HomeView(TemplateView):
 	template_name = 'home/home.html'
@@ -10,8 +11,19 @@ class HomeView(TemplateView):
 	def get(self, request):
 		form = HomeForm()
 		posts = Post.objects.all().order_by('-created_date', '-updated_date')
-		users = User.objects.all()
-		args = {'form': form, 'posts': posts, 'users': users,}
+		users = User.objects.exclude(id=request.user.id)
+		if request.user.is_authenticated:
+			friend = Friend.objects.get(current_user=request.user)
+			friends = friend.users.all()
+		else:
+			friend = None
+			friends = None
+		args = {
+			'form': form, 
+			'posts': posts, 
+			'users': users, 
+			'friends': friends,
+		}
 		return render(request, self.template_name, args)
 		
 	def post(self, request):
@@ -25,3 +37,14 @@ class HomeView(TemplateView):
 			return redirect('home')
 		args = {'form': form, 'text': text}
 		return render(request, self.template_name, args)
+		
+def change_friends(request, operation, pk):		
+	friend = User.objects.get(pk=pk)
+	if operation == 'add':
+		Friend.add_friend(request.user, friend)
+	elif operation == 'remove':
+		Friend.remove_friend(request.user, friend)
+	else:
+		return redirect('home')
+	return redirect('home')
+			
