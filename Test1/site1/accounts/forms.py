b03bad6c869401fd1500140b8311e6df575a345f
Mingzhi Xu <mingzhi22@yahoo.com>
2019-04-29 07:53:43 -0400

    Add files via upload

--- Test1/site1/accounts/forms.py
+++ Test1/site1/accounts/forms.py
@@ -7,49 +7,39 @@ class RegistrationForm(UserCreationForm):
 	first_name = forms.CharField(required=True)
 	last_name = forms.CharField(required=True)
 	email = forms.EmailField(required=True)
-	address = forms.CharField(required=True)
-	credit_card_number = forms.CharField(required=True)
-	phone_number = forms.CharField(required=True)
 	class Meta:
-		model = UserProfile
+		model = User
 		fields = (
 			'username',
 			'first_name',
 			'last_name',
 			'email',
-			'address',
-			'credit_card_number',
-			'phone_number',
 			'password1',
 			'password2',
 			)
 			
 		def save(self, commit=True):
-			userprofile = super(RegistrationForm, self).save(commit=False)
-			userprofile.first_name = self.cleaned_data['first_name']
-			userprofile.last_name = self.cleaned_data['last_name']
-			userprofile.email = self.cleaned_data['email']
-			userprofile.address = self.cleaned_data['address']
-			userprofile.credit_card_number = self.cleaned_data['credit_card_number']
-			userprofile.phone_number = self.cleaned_data['phone_number']
-			
+			UserProfile = super(UserCreationForm, self).save(commit=False)
+			UserProfile.username = self.cleaned_data['username']
+			UserProfile.first_name = self.cleaned_data['first_name']
+			UserProfile.last_name = self.cleaned_data['last_name']
+			UserProfile.email = self.cleaned_data['email']
+			UserProfile.address = self.cleaned_data['address']
+			UserProfile.credit_card_number = self.cleaned_data['credit_card_number']
+			UserProfile.phone_number = self.cleaned_data['phone_number']
 			if commit:
-				userprofile.save()
+				UserProfile.save()
 			
-			return userprofile
+			return UserProfile
 			
 class EditProfileForm(UserChangeForm):
 	
 	class Meta:
-		model = UserProfile
+		model = User
 		fields = (
 		'first_name',
 		'last_name',
-		'email',	
-		'address',
-		'credit_card_number',
-		'phone_number',
-		'image',
+		'email',
 		'password',
 		)
 		
--- Test1/site1/accounts/models.py
+++ Test1/site1/accounts/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from home.models import Friend
+
+
 # Create your models here.
-class UserProfileManager(models.Manager):
-	def get_queryset(self):
-		return super(UserProfileManager, self).get_queryset()
 		
 class UserProfile(models.Model):
-	username = models.OneToOneField(User, on_delete=models.CASCADE)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	username = models.CharField(max_length=100, default='')
 	first_name = models.CharField(max_length=100, default='')
 	last_name = models.CharField(max_length=100, default='')
 	email = models.EmailField(max_length=100, default='')
@@ -17,12 +18,13 @@ class UserProfile(models.Model):
 	credit_card_number = models.CharField(max_length=100, default='')
 	phone_number = models.CharField(max_length=100, default='')
 	image = models.ImageField(upload_to='profile_image', blank=True)
-	
+
 	USERNAME_FIELD = 'username'
 	def __str__(self):
-		return self.username.username
+		return self.user.username
 
-	@receiver(post_save, sender=User)
-	def create_user_profile(sender, instance, created, **kwargs):
-		if created:
-			Profile.objects.create(username=instance)
+def create_profile(sender, **kwargs):
+	if kwargs['created']:
+		user_profile = UserProfile.objects.create(user=kwargs['instance'])
+		
+post_save.connect(create_profile, sender=User)
