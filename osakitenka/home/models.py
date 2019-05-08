45c962bf63c76a66dccb5606b5dd2c7d8ebf5263
William <willchung42@gmail.com>
2019-05-08 16:15:51 -0400

    fixed directories

---
+++ osakitenka/home/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+	post = models.CharField(max_length=500)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	created_date = models.DateTimeField(auto_now_add=True)
+	updated_date = models.DateTimeField(auto_now=True)
+	
+class Friend(models.Model):
+	users = models.ManyToManyField(User, blank=True)
+	current_user = models.ForeignKey(User, related_name='owner', null=True, on_delete=models.CASCADE)
+	@classmethod
+	def add_friend(cls, current_user, new_friend):
+		friend, created = cls.objects.get_or_create(
+			current_user = current_user
+		)
+		friend.users.add(new_friend)
+		
+	@classmethod
+	def remove_friend(cls, current_user, new_friend):
+		friend, created = cls.objects.get_or_create(
+			current_user = current_user
+		)
+		friend.users.remove(new_friend)
